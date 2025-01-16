2411f1d717efc04b50f372ce6f9c3d3b16cdf922
Recep Aslantas <info@recp.me>
2025-01-16 17:18:42 +0300

    adam7

--- src/io/png/png.c
+++ src/io/png/png.c
@@ -15,6 +15,8 @@
  */
 
 #include "png.h"
+#include <defl/infl.h>
+
 #include "../../file.h"
 #include "../../endian.h"
 #include "../../zz/zz.h"
@@ -23,27 +25,132 @@
                              + ((unsigned)(c) << 8)  + (unsigned)(d))
 
 typedef enum im_png_filter_t {
-  IM_PNG_FILTER_NONE  = 0,
-  IM_PNG_FILTER_SUB   = 1,
-  IM_PNG_FILTER_UP    = 2,
-  IM_PNG_FILTER_AVG   = 3,
-  IM_PNG_FILTER_PAETH = 4
+  FILT_NONE  = 0,
+  FILT_SUB   = 1,
+  FILT_UP    = 2,
+  FILT_AVG   = 3,
+  FILT_PAETH = 4
 } im_png_filter_t;
 
 IM_INLINE ImByte mod256u8(ImByte b) { return b & 0xFF; }
 IM_INLINE int    mod256i(int b)     { return b & 0xFF; }
 
+
+//#if defined(__ARM_NEON)
+//#  include "arch/neon.h"
+//#elif defined(__x86_64__) || defined(_M_X64)
+//#  include "arch/x86.h"
+//#endif
+
 IM_INLINE int paeth(int a, int b, int c) {
-  int p, pa, pb, pc;
+  int p  = a + b - c;
+  int pa = abs(p - a);
+  int pb = abs(p - b);
+  int pc = abs(p - c);
 
-  p  = a + b - c;
-  pa = abs(p - a);
-  pb = abs(p - b);
-  pc = abs(p - c);
+  return (pa <= pb && pa <= pc) ? a : (pb <= pc ? b : c);
+}
 
-  if (pa <= pb && pa <= pc) return a;
-  else if (pb <= pc)        return b;
-  else                      return c;
+static
+void
+undo_filters(ImByte *pass_data, uint32_t pass_width, uint32_t bpp, uint32_t pass_height) {
+  ImByte  *ptr, *row, *pri;
+  uint32_t bpr, x, y;
+
+  bpr = pass_width * bpp;
+  row = pri = pass_data;
+  ptr = row + 1;
+
+  /* handle the first row as a special case to improve other rows */
+  switch (row[0]) {
+    case FILT_SUB:   for(x=bpp; x<bpr; x++) ptr[x] += ptr[x-bpp];            break;
+    case FILT_AVG:   for(x=bpp; x<bpr; x++) ptr[x] += ptr[x-bpp] >> 1;       break;
+    case FILT_PAETH: for(x=bpp; x<bpr; x++) ptr[x] += paeth(ptr[x-bpp],0,0); break;
+  }
+
+  /* move to the next row */
+  pri  = row;
+  row += bpr + 1;
+  ptr  = row + 1;
+
+  /* process remaining rows */
+  for (y = 1; y < pass_height; y++) {
+    switch (row[0]) {
+      case FILT_SUB:
+        for (x=bpp; x<bpr; x++) ptr[x] += ptr[x-bpp];
+        break;
+      case FILT_UP:
+        for (x=0; x<bpr; x++)   ptr[x] += pri[x+1];
+        break;
+      case FILT_AVG:
+        for (x=0;   x<bpp; x++) ptr[x] += pri[x+1] >> 1;
+        for (x=bpp; x<bpr; x++) ptr[x] += ((uint16_t)ptr[x-bpp] + pri[x+1]) >> 1;
+        break;
+      case FILT_PAETH:
+        for (x=0;   x<bpp; x++) ptr[x] += paeth(0, pri[x+1], 0);
+        for (x=bpp; x<bpr; x++) ptr[x] += paeth(ptr[x-bpp], pri[x+1], pri[x-bpp+1]);
+        break;
+    }
+    /* move to the next row */
+    pri  = row;
+    row += bpr + 1;
+    ptr  = row + 1;
+  }
+}
+
+static
+ImByte*
+deinterlace_adam7(ImImage *im, ImByte *src) {
+  const uint8_t x_start[7]  = {0,4,0,2,0,1,0};
+  const uint8_t y_start[7]  = {0,0,4,0,2,0,1};
+  const uint8_t x_delta[7]  = {8,8,4,4,2,2,1};
+  const uint8_t y_delta[7]  = {8,8,8,4,4,2,2};
+
+  ImByte  *dest;
+  ImByte  *pass_data, *src_row, pass;
+  uint32_t width, height, pass_w, pass_h, bpp, stride, x, y, dest_x, dest_y;
+
+  width  = im->width;
+  height = im->height;
+  bpp    = im->bytesPerPixel;
+
+  /* TODO: calloc vs malloc */
+  if (!(dest = calloc(1, width * height * bpp))) return NULL;
+
+  pass_data = src;
+
+  /* process each pass */
+  for (pass = 0; pass < 7; pass++) {
+    pass_w = (width  - x_start[pass] + x_delta[pass] - 1) / x_delta[pass];
+    pass_h = (height - y_start[pass] + y_delta[pass] - 1) / y_delta[pass];
+
+    if (!pass_w || !pass_h)
+      continue;
+
+    /* process this pass's filters */
+    undo_filters(pass_data, pass_w, bpp, pass_h);
+
+    /* Copy pixels to final positions */
+    stride = pass_w * bpp + 1;
+    for (y = 0; y < pass_h; y++) {
+      src_row = pass_data + y * stride + 1;  /* skip filter byte */
+      dest_y  = y * y_delta[pass] + y_start[pass];
+
+      if (dest_y >= height)
+        continue;
+
+      for (x = 0; x < pass_w; x++) {
+        dest_x = x * x_delta[pass] + x_start[pass];
+        if (dest_x < width)
+          memcpy(&dest[(dest_y * width + dest_x) * bpp], &src_row[x * bpp], bpp);
+      }
+    }
+
+    /* move to next pass */
+    pass_data += pass_h * (pass_w * bpp + 1);
+  }
+
+  return dest;
 }
 
 IM_HIDE
@@ -51,33 +158,36 @@ ImResult
 png_dec(ImImage         ** __restrict dest,
         const char       * __restrict path,
         im_open_config_t * __restrict open_config) {
-  unzip_t        *zip;
+  infl_stream_t  *imdefl;
   ImImage        *im;
   ImByte         *zipped;
   ImByte         *p, *p_chk, *row, *pri, bitdepth, color, compr, interlace;
   im_png_filter_t filter;
-  uint32_t        len, chk_len, chk_type, pal_len, i, j, width, height, src_bpr, bpp, bpc, zippedlen;
+  size_t          len;
+  uint32_t        chk_len, chk_type, pal_len, i, j, width, height, src_bpr, bpp, bpc, zippedlen;
   uint16_t       *p16;
   ImFileResult    fres;
   bool            is_cgbi;
 
-  im     = NULL;
-  row    = NULL;
-  zipped = NULL;
-  fres   = im_readfile(path, open_config->openIntent != IM_OPEN_INTENT_READWRITE);
+  im        = NULL;
+  row       = NULL;
+  zipped    = NULL;
+  imdefl    = NULL;
+  interlace = 0;
+  fres      = im_readfile(path, open_config->openIntent != IM_OPEN_INTENT_READWRITE);
 
   if (fres.ret != IM_OK) {
     goto err;
   }
-  
+
   /* decode, this process will be optimized after decoding is done */
   im  = calloc(1, sizeof(*im));
   p   = fres.raw;
-  
+
   /*
-     Magic number types:
-     -------------------
-     89 50 4E 47 0D 0A 1A 0A
+   Magic number types:
+   -------------------
+   89 50 4E 47 0D 0A 1A 0A
    */
 
   if (p[0] != 0x89 ||
@@ -129,7 +239,7 @@ png_dec(ImImage         ** __restrict dest,
          4       8,16        Each pixel is a grayscale sample, followed by an alpha sample.
          6       8,16        Each pixel is an R,G,B triple, followed by an alpha sample.
          */
-        
+
         switch (color) {
           case 0:
             im->bitsPerPixel       = bitdepth;
@@ -176,20 +286,24 @@ png_dec(ImImage         ** __restrict dest,
           default:
             goto err;  /* invalid color type */
         }
-        
+
         /* TODO: */
+        // im->len       = len = (bpp + im->row_pad_last) * (width + 1) * height;
+        //        im->len       = len = (bpp * width + im->row_pad_last + 1) * height;
         im->len       = len = (bpp + im->row_pad_last) * (width + 1) * height;
         im->data.data = malloc(len);
         zipped        = row = malloc(len);
+
+        imdefl        = infl_init(im->data.data, (uint32_t)im->len, 1);
         break;
       }
       case IM_PNG_TYPE('P','L','T','E'): {
         if (im->pal) {
           ImByte *pal;
-          
+
           if (chk_len > 256 * 3 || (pal_len = chk_len / 3) * 3 != chk_len)
             goto err; /* invalid PLTE corrupt PNG */
-          
+
           im->pal->len   = chk_len;
           im->pal->pal   = pal = malloc(chk_len);
           im->pal->white = bitdepth;
@@ -208,6 +322,8 @@ png_dec(ImImage         ** __restrict dest,
         memcpy(row, p, chk_len);
         row       += chk_len;
         zippedlen += chk_len;
+
+        //        infl_include(imdefl, p, chk_len);
         break;
       }
       case IM_PNG_TYPE('I','E','N','D'): {
@@ -221,8 +337,28 @@ png_dec(ImImage         ** __restrict dest,
 nx:
 
   /* TODO: */
-  mz_uncompress(im->data.data, &len, zipped, zippedlen);
+  //   mz_uncompress(im->data.data, &len, zipped, zippedlen);
+  //  decompress_idat(zipped, zippedlen, im->data.data, im->len);
+  //  zsinflate(im->data.data, (int)im->len, zipped, zippedlen);
 
+  defl_include(imdefl, zipped, zippedlen);
+  if (infl(imdefl)) {
+    goto err;
+  }
+
+  if (unlikely(interlace)) {
+    ImByte *deinterlaced;
+
+    if (!(deinterlaced = deinterlace_adam7(im, im->data.data)))
+      goto err;
+
+    free(im->data.data);
+    im->data.data = deinterlaced;
+
+    goto af;
+  }
+
+  /* non-interlaced: undo filter in the usual way */
   src_bpr = bpp * width * ((float)im_minu8(bitdepth, 8) / 8.0f);
   row     = p = im->data.data;
   pri     = p;
@@ -231,57 +367,39 @@ nx:
   /* undo filter */
 
   switch ((int)*row) {
-    case IM_PNG_FILTER_UP:
+    case FILT_UP:
       memmove(p, row + 1, src_bpr);
       goto nx_row;
-    case IM_PNG_FILTER_AVG:
+    case FILT_AVG:
       im_pixcpy(p, row + 1, bpp);
-      for (j = bpp; j < src_bpr; j++) {
-        p[j] = row[j + 1] + (p[j - bpp] >> 1);
-      }
+      for (j = bpp; j < src_bpr; j++) p[j] = row[j+1] + (p[j-bpp] >> 1);
       goto nx_row;
-    case IM_PNG_FILTER_PAETH:
+    case FILT_PAETH:
       im_pixcpy(p, row + 1, bpp);
-      for (j = bpp; j < src_bpr; j++) {
-        p[j] = row[j + 1] + paeth(p[j - bpp], 0, 0);
-      }
+      for (j = bpp; j < src_bpr; j++) p[j] = row[j+1] + paeth(p[j-bpp], 0, 0);
       goto nx_row;
     default: break;
   }
 
   for (; i < height; ) {
     switch (row[0]) {
-      case IM_PNG_FILTER_NONE:
+      case FILT_NONE:
         memmove(row - i, row + 1, src_bpr);
         break;
-      case IM_PNG_FILTER_SUB:
+      case FILT_SUB:
         memmove(p, row + 1, bpp);
-        for (j = bpp; j < src_bpr; j++) {
-          p[j] = row[j + 1] + p[j - bpp];
-        }
+        for (j=bpp; j<src_bpr; j++) p[j] = row[j+1] + p[j - bpp];
         break;
-      case IM_PNG_FILTER_UP:
-        for (j = 0; j < src_bpr; j++) {
-          p[j] = row[j + 1] + pri[j];
-        }
+      case FILT_UP:
+        for (j=0; j<src_bpr; j++)   p[j] = row[j+1] + pri[j];
         break;
-      case IM_PNG_FILTER_AVG:
-        for (j = 0; j < bpp; j++) {
-          p[j] = row[j + 1] + ((pri[j]) >> 1);
-        }
-
-        for (j = bpp; j < src_bpr; j++) {
-          p[j] = row[j + 1] + ((p[j - bpp] + pri[j]) >> 1);
-        }
+      case FILT_AVG:
+        for (j=0;   j<bpp;     j++) p[j] = row[j+1] + ((pri[j]) >> 1);
+        for (j=bpp; j<src_bpr; j++) p[j] = row[j+1] + ((p[j-bpp] + pri[j]) >> 1);
         break;
-      case IM_PNG_FILTER_PAETH:
-        for (j = 0; j < bpp; j++) {
-          p[j] = row[j + 1] + paeth(0, pri[j], 0);
-        }
-
-        for (j = bpp; j < src_bpr; j++) {
-          p[j] = row[j + 1] + paeth(p[j - bpp], pri[j], pri[j - bpp]);
-        }
+      case FILT_PAETH:
+        for (j=0;   j<bpp; j++)     p[j] = row[j+1] + paeth(0, pri[j], 0);
+        for (j=bpp; j<src_bpr; j++) p[j] = row[j+1] + paeth(p[j-bpp], pri[j], pri[j-bpp]);
         break;
       default:
         goto err; /* unknown or unimplemented filter */
@@ -294,6 +412,7 @@ nx:
     i++;
   }
 
+af:
   /* fix byte order */
   if (unlikely(bpc > 1)) {
     switch (open_config->byteOrder) {
@@ -326,13 +445,16 @@ nx:
   im->file = fres;
   *dest    = im;
 
+  //  unzip_cleanup(zip);
+
   return IM_OK;
 
 err:
   if (fres.mmap) { im_unmap(fres.raw, fres.size); }
   if (im)        { free(im);                      }
-  
   *dest = NULL;
+
+  infl_destroy(imdefl);
 
   return IM_ERR;
 }
